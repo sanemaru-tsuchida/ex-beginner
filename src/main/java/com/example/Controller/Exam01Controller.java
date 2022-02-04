6eb6827dad2e5a00d3af7f2d568e52ebe1850fea
TsuchidaSanemaru <sanemaru.tsuchida@rakus-partners.co.jp>
2022-02-04 13:14:01 +0900

    Exam2 終了
    Exam02Controller 終了
    
    exam02.html 終了
    
    exam02-result.html 終了
    
    exam02-result2.html 終了

--- src/main/java/com/example/Controller/Exam01Controller.java
+++ src/main/java/com/example/Controller/Exam01Controller.java
@@ -4,6 +4,7 @@ package com.example.Controller;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.From.Exam1;
@@ -13,6 +14,10 @@ import com.example.From.Exam1;
 @Controller
 @RequestMapping("/ex1")
 public class Exam01Controller {
+	@ModelAttribute
+	public Exam1 index2() {
+		return new Exam1();
+	}
 	
 	@RequestMapping("")
 	public String index() {
---
+++ src/main/java/com/example/Controller/Exam02Controller.java
@@ -0,0 +1,38 @@
+package com.example.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.From.Exam2;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/ex2")
+public class Exam02Controller {
+	@ModelAttribute
+	public Exam2 setUpForm() {
+		return new Exam2();
+	}
+	@Autowired
+	private HttpSession session;
+	@RequestMapping("")
+	public String idex2() {
+		return "exam02";
+	}
+	@RequestMapping("/input")
+	public String index3(Exam2 exam2) {
+		Integer num1 = Integer.parseInt(exam2.getNum1());
+		Integer num2 = Integer.parseInt(exam2.getNum2());
+		Integer answer = num1 + num2;
+		session.setAttribute("ansewer", answer);
+		session.setAttribute("exam2", exam2);
+		return "exam02-result";
+	}
+	@RequestMapping("/to-page2")
+	public String idex4() {
+		return "exam02-result2";
+	}
+}
